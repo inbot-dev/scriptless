807bedfc15beb88bc2baed1ca42aec26c6c4c8ed
inbot-dev <wildansyarif91@gmail.com>
2025-11-22 23:57:53 -0600

    script rev

--- vultr/bekap.sh
+++ vultr/bekap.sh
@@ -6,13 +6,13 @@ pm2 restart notif
 
 cd
 
-curl -s -X POST https://api.telegram.org/bot1899238854:AAH0tUYroXQLLmDw-sED2lzlE-ao2UpUbzM/sendMessage -d chat_id=277081400 -d text="Backup Vultr Mulai"
+# curl -s -X POST https://api.telegram.org/bot1899238854:AAH0tUYroXQLLmDw-sED2lzlE-ao2UpUbzM/sendMessage -d chat_id=277081400 -d text="Backup Vultr Mulai"
 
 mkdir -p /root/gd_bu
 chmod +x /root/gd_bu
 
 [ -d '/root/gunbot.my.id' ] && (
-    curl -s -X POST https://api.telegram.org/bot1899238854:AAH0tUYroXQLLmDw-sED2lzlE-ao2UpUbzM/sendMessage -d chat_id=277081400 -d text="Backup Folder Core Gunbot Mulai"
+    # curl -s -X POST https://api.telegram.org/bot1899238854:AAH0tUYroXQLLmDw-sED2lzlE-ao2UpUbzM/sendMessage -d chat_id=277081400 -d text="Backup Folder Core Gunbot Mulai"
     mkdir -p /root/gd_bu/gunbot.my.id
     chmod +x /root/gd_bu/gunbot.my.id
     \cp -avrf /root/gunbot.my.id/* /root/gd_bu/gunbot.my.id/
@@ -22,11 +22,11 @@ chmod +x /root/gd_bu
     mkdir -p /root/bekap_json/gunbot.my.id
     chmod +x /root/bekap_json/gunbot.my.id
     \cp -avrf /root/gunbot.my.id/json/* /root/bekap_json/gunbot.my.id/
-    curl -s -X POST https://api.telegram.org/bot1899238854:AAH0tUYroXQLLmDw-sED2lzlE-ao2UpUbzM/sendMessage -d chat_id=277081400 -d text="Backup Folder Core Gunbot Selesai"
+    # curl -s -X POST https://api.telegram.org/bot1899238854:AAH0tUYroXQLLmDw-sED2lzlE-ao2UpUbzM/sendMessage -d chat_id=277081400 -d text="Backup Folder Core Gunbot Selesai"
 )
 
 [ -d '/root/bitrage.gunbot.my.id' ] && (
-    curl -s -X POST https://api.telegram.org/bot1899238854:AAH0tUYroXQLLmDw-sED2lzlE-ao2UpUbzM/sendMessage -d chat_id=277081400 -d text="Backup Folder Bitrage Gunbot Mulai"
+    # curl -s -X POST https://api.telegram.org/bot1899238854:AAH0tUYroXQLLmDw-sED2lzlE-ao2UpUbzM/sendMessage -d chat_id=277081400 -d text="Backup Folder Bitrage Gunbot Mulai"
     mkdir -p /root/gd_bu/bitrage.gunbot.my.id
     chmod +x /root/gd_bu/bitrage.gunbot.my.id
     \cp -avrf /root/bitrage.gunbot.my.id/* /root/gd_bu/bitrage.gunbot.my.id/
@@ -36,47 +36,47 @@ chmod +x /root/gd_bu
     mkdir -p /root/bekap_json/bitrage.gunbot.my.id
     chmod +x /root/bekap_json/bitrage.gunbot.my.id
     \cp -avrf /root/bitrage.gunbot.my.id/json/* /root/bekap_json/bitrage.gunbot.my.id/
-    curl -s -X POST https://api.telegram.org/bot1899238854:AAH0tUYroXQLLmDw-sED2lzlE-ao2UpUbzM/sendMessage -d chat_id=277081400 -d text="Backup Folder Bitrage Gunbot Selesai"
+    # curl -s -X POST https://api.telegram.org/bot1899238854:AAH0tUYroXQLLmDw-sED2lzlE-ao2UpUbzM/sendMessage -d chat_id=277081400 -d text="Backup Folder Bitrage Gunbot Selesai"
 )
 
 [ -d '/root/tools' ] && (
-    curl -s -X POST https://api.telegram.org/bot1899238854:AAH0tUYroXQLLmDw-sED2lzlE-ao2UpUbzM/sendMessage -d chat_id=277081400 -d text="Backup Folder Tools Mulai"
+    # curl -s -X POST https://api.telegram.org/bot1899238854:AAH0tUYroXQLLmDw-sED2lzlE-ao2UpUbzM/sendMessage -d chat_id=277081400 -d text="Backup Folder Tools Mulai"
     mkdir -p /root/gd_bu/tools
     chmod +x /root/gd_bu/tools
     \cp -avrf /root/tools/* /root/gd_bu/tools/
     zip -r /root/gd_bu/tools.zip /root/gd_bu/tools
-    curl -s -X POST https://api.telegram.org/bot1899238854:AAH0tUYroXQLLmDw-sED2lzlE-ao2UpUbzM/sendMessage -d chat_id=277081400 -d text="Backup Folder Tools Selesai"
+    # curl -s -X POST https://api.telegram.org/bot1899238854:AAH0tUYroXQLLmDw-sED2lzlE-ao2UpUbzM/sendMessage -d chat_id=277081400 -d text="Backup Folder Tools Selesai"
 )
 
 [ -d '/root/idena'] && (
-    curl -s -X POST https://api.telegram.org/bot1899238854:AAH0tUYroXQLLmDw-sED2lzlE-ao2UpUbzM/sendMessage -d chat_id=277081400 -d text="Backup Idena Mulai"
+    # curl -s -X POST https://api.telegram.org/bot1899238854:AAH0tUYroXQLLmDw-sED2lzlE-ao2UpUbzM/sendMessage -d chat_id=277081400 -d text="Backup Idena Mulai"
     mkdir -p /root/gd_bu/idena
     chmod +x /root/gd_bu/idena
     \cp -avrf /root/idena/* /root/gd_bu/idena/
     zip -r /root/gd_bu/idena.zip /root/gd_bu/idena
-    curl -s -X POST https://api.telegram.org/bot1899238854:AAH0tUYroXQLLmDw-sED2lzlE-ao2UpUbzM/sendMessage -d chat_id=277081400 -d text="Backup Idena Selesai"
+    # curl -s -X POST https://api.telegram.org/bot1899238854:AAH0tUYroXQLLmDw-sED2lzlE-ao2UpUbzM/sendMessage -d chat_id=277081400 -d text="Backup Idena Selesai"
 )
 
-curl -s -X POST https://api.telegram.org/bot1899238854:AAH0tUYroXQLLmDw-sED2lzlE-ao2UpUbzM/sendMessage -d chat_id=277081400 -d text="Backup Semua Vultr Config RClone Mulai"
+# curl -s -X POST https://api.telegram.org/bot1899238854:AAH0tUYroXQLLmDw-sED2lzlE-ao2UpUbzM/sendMessage -d chat_id=277081400 -d text="Backup Semua Vultr Config RClone Mulai"
 mkdir -p /root/gd_bu/.config
 chmod +x /root/gd_bu/.config
 \cp -avrf /root/.config/* /root/gd_bu/.config/
 zip -r /root/gd_bu/.config.zip /root/gd_bu/.config
-curl -s -X POST https://api.telegram.org/bot1899238854:AAH0tUYroXQLLmDw-sED2lzlE-ao2UpUbzM/sendMessage -d chat_id=277081400 -d text="Backup Semua Vultr Config RClone Selesai"
+# curl -s -X POST https://api.telegram.org/bot1899238854:AAH0tUYroXQLLmDw-sED2lzlE-ao2UpUbzM/sendMessage -d chat_id=277081400 -d text="Backup Semua Vultr Config RClone Selesai"
 
-curl -s -X POST https://api.telegram.org/bot1899238854:AAH0tUYroXQLLmDw-sED2lzlE-ao2UpUbzM/sendMessage -d chat_id=277081400 -d text="Backup Semua Vultr File SH Mulai"
+# curl -s -X POST https://api.telegram.org/bot1899238854:AAH0tUYroXQLLmDw-sED2lzlE-ao2UpUbzM/sendMessage -d chat_id=277081400 -d text="Backup Semua Vultr File SH Mulai"
 mkdir -p /root/gd_bu/.sh
 chmod +x /root/gd_bu/.sh
 \cp -avrf /root/*.sh /root/gd_bu/.sh/
 zip -r /root/gd_bu/.sh.zip /root/gd_bu/.sh
-curl -s -X POST https://api.telegram.org/bot1899238854:AAH0tUYroXQLLmDw-sED2lzlE-ao2UpUbzM/sendMessage -d chat_id=277081400 -d text="Backup Semua Vultr File SH Selesai"
+# curl -s -X POST https://api.telegram.org/bot1899238854:AAH0tUYroXQLLmDw-sED2lzlE-ao2UpUbzM/sendMessage -d chat_id=277081400 -d text="Backup Semua Vultr File SH Selesai"
 
 rclone sync --progress --drive-acknowledge-abuse --local-no-check-updated --checksum --metadata /root/gd_bu/ ws-onedrive:Server/Backup/Vultr
 
 rm -rf /root/bekap_json/
 rm -rf /root/gd_bu
 
-curl -s -X POST https://api.telegram.org/bot1899238854:AAH0tUYroXQLLmDw-sED2lzlE-ao2UpUbzM/sendMessage -d chat_id=277081400 -d text="Backup Vultr Selesai"
+# curl -s -X POST https://api.telegram.org/bot1899238854:AAH0tUYroXQLLmDw-sED2lzlE-ao2UpUbzM/sendMessage -d chat_id=277081400 -d text="Backup Vultr Selesai"
 
 sudo apt update
 sudo apt upgrade -y
