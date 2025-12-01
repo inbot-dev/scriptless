87063660b5e8de66604d9c234aa7c2962fde45fe
inbot-dev <wildansyarif91@gmail.com>
2025-11-30 18:37:22 -0600

    rclone

--- contabo/ws_work_wss-one.sh
+++ contabo/ws_work_wss-one.sh
@@ -2,7 +2,7 @@
 
 cd
 if ! screen -list | grep -q "ws_work_wss-one"; then
-    screen -dmS ws_work_wss-one rclone sync --local-no-check-updated --drive-acknowledge-abuse --progress --checksum --metadata ws-onedrive_work:IE/S2 wdjp-gdrive:S2
+    screen -dmS ws_work_wss-one rclone sync --local-no-check-updated --drive-acknowledge-abuse --progress --checksum --metadata ws-onedrive_work:IE/S2 ws-onedrive:S2
 fi
 
 exit 0
--- contabo/ws_work_wss-one_de.sh
+++ contabo/ws_work_wss-one_de.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 cd
-if ! screen -list | grep -q "ws_work_wdjp-g"; then
-    screen -dmS ws_work_wdjp-g_de rclone dedupe --by-hash --dedupe-mode newest ws-onedrive_work:
+if ! screen -list | grep -q "ws_work_wss-one_de"; then
+    screen -dmS ws_work_wss-one_de rclone dedupe --by-hash --dedupe-mode newest ws-onedrive_work:
 fi
 
 exit 0
