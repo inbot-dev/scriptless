ec4b1526e414c2ad08bb37b192aa0673fff8aa71
inbot-dev <wildansyarif91@gmail.com>
2025-12-09 07:55:35 -0600

    wss

--- contabo/ws_work_wss-one.sh
+++ contabo/ws_work_wss-one.sh
@@ -10,7 +10,7 @@ if ! screen -list | grep -q "ws_work_wss-one"; then
             sleep 5
         done
 
-        screen -dmS ws_work_wss-one rclone sync --local-no-check-updated --drive-acknowledge-abuse --progress --checksum --metadata ws-onedrive_work:IE/S2 ws-onedrive:S2
+        screen -dmS ws_work_wss-one rclone sync --local-no-check-updated --drive-acknowledge-abuse --progress --checksum --metadata ws-onedrive_work:IE/S2 wss-onedrive:S2
     fi
 fi
 
